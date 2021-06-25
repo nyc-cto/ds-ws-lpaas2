c9c8fc7eb675817d056c81ca28b94968e52a65f8
Anya <adunaif@cto.nyc.gov>
2021-06-25 13:49:10 -0400

    content updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: "LPaaS 2.0",
   },
   plugins: [
     {
--- src/markdown-pages/content-1.md
+++ src/markdown-pages/content-1.md
@@ -3,3 +3,4 @@ slug: "/content-1"
 date: "2021-06-25"
 title: "NYC Content 1"
 ---
+# Hello
