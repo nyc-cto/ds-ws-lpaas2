8cdeac29b27563cdb253dc2d9dd5c682c4b2d80d
Anya <adunaif@cto.nyc.gov>
2021-06-30 13:47:35 -0400

    working on index page template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,63 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { GridContainer } from "@trussworks/react-uswds";
-import { Graphic, Header, Hero, Section, Tagline } from "../components/";
-import "@trussworks/react-uswds/lib/uswds.css";
-import "@trussworks/react-uswds/lib/index.css";
+// import React from "react";
+// import { graphql } from "gatsby";
+// import { GridContainer } from "@trussworks/react-uswds";
+// import { Graphic, Header, Hero, Section, Tagline } from "../components/";
+// import "@trussworks/react-uswds/lib/uswds.css";
+// import "@trussworks/react-uswds/lib/index.css";
 
-const Home = ({ data }) => {
-  // const { markdownRemark } = data;
-  // const { frontmatter } = markdownRemark;
-  return (
-    <div>
-      <Header />
-      {/* <main>
-        <GridContainer>
-          <Hero hero={frontmatter.hero} buttons={frontmatter.buttons} />
-          <Tagline tagline={frontmatter.tagline} />
-          <Graphic graphics={frontmatter.graphics} />
-          <Section
-            section={frontmatter.section}
-            buttons={frontmatter.buttons}
-          />
-        </GridContainer>
-      </main> */}
-    </div>
-  );
-};
+// const Home = ({ data }) => {
+//   // const { markdownRemark } = data;
+//   // const { frontmatter } = markdownRemark;
+//   return (
+//     <div>
+//       <Header />
+//       {/* <main>
+//         <GridContainer>
+//           <Hero hero={frontmatter.hero} buttons={frontmatter.buttons} />
+//           <Tagline tagline={frontmatter.tagline} />
+//           <Graphic graphics={frontmatter.graphics} />
+//           <Section
+//             section={frontmatter.section}
+//             buttons={frontmatter.buttons}
+//           />
+//         </GridContainer>
+//       </main> */}
+//     </div>
+//   );
+// };
 
-export const pageQuery = graphql`
-  query Home($lang: String!) {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" }, lang: { eq: $lang }  }) {
-      frontmatter {
-        lang
-        slug
-        hero {
-          image
-          imageDescription
-          heading
-          text
-        }
-        buttons {
-          callToAction
-        }
-        tagline {
-          heading
-          text
-        }
-        graphics {
-          image
-          imageDescription
-          heading
-          text
-        }
-        section {
-          heading
-          text
-        }
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query Home($lang: String!) {
+//     markdownRemark(frontmatter: { templateKey: { eq: "index-page" }, lang: { eq: $lang }  }) {
+//       frontmatter {
+//         lang
+//         slug
+//         hero {
+//           image
+//           imageDescription
+//           heading
+//           text
+//         }
+//         buttons {
+//           callToAction
+//         }
+//         tagline {
+//           heading
+//           text
+//         }
+//         graphics {
+//           image
+//           imageDescription
+//           heading
+//           text
+//         }
+//         section {
+//           heading
+//           text
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export default Home;
+// export default Home;
--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,60 +1,60 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { GridContainer } from "@trussworks/react-uswds";
-import { Graphic, Hero, Section, Tagline } from "../components/";
-import "@trussworks/react-uswds/lib/uswds.css";
-import "@trussworks/react-uswds/lib/index.css";
+// import React from "react";
+// import { graphql } from "gatsby";
+// import { GridContainer } from "@trussworks/react-uswds";
+// import { Graphic, Hero, Section, Tagline } from "../components/";
+// import "@trussworks/react-uswds/lib/uswds.css";
+// import "@trussworks/react-uswds/lib/index.css";
 
-const Template = ({ data }) => {
-  const { markdownRemark } = data;
-  const { frontmatter } = markdownRemark;
-  return (
-    <main>
-      <GridContainer>
-        <Hero
-          hero={frontmatter.hero}
-          buttons={frontmatter.buttons}
-        />
-        <Tagline tagline={frontmatter.tagline} />
-        <Graphic graphics={frontmatter.graphics} />
-        <Section section={frontmatter.section} buttons={frontmatter.buttons} />
-      </GridContainer>
-    </main>
-  );
-};
+// const Template = ({ data }) => {
+//   const { markdownRemark } = data;
+//   const { frontmatter } = markdownRemark;
+//   return (
+//     <main>
+//       <GridContainer>
+//         <Hero
+//           hero={frontmatter.hero}
+//           buttons={frontmatter.buttons}
+//         />
+//         <Tagline tagline={frontmatter.tagline} />
+//         <Graphic graphics={frontmatter.graphics} />
+//         <Section section={frontmatter.section} buttons={frontmatter.buttons} />
+//       </GridContainer>
+//     </main>
+//   );
+// };
 
-export const pageQuery = graphql`
-  query ($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      frontmatter {
-        lang
-        slug
-        hero {
-          image
-          imageDescription
-          heading
-          text
-        }
-        buttons {
-          callToAction
-        }
-        tagline {
-          heading
-          text
-        }
-        graphics {
-          image
-          imageDescription
-          heading
-          text
-        }
-        section {
-          heading
-          text
-        }
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query ($id: String!) {
+//     markdownRemark(id: { eq: $id }) {
+//       frontmatter {
+//         lang
+//         slug
+//         hero {
+//           image
+//           imageDescription
+//           heading
+//           text
+//         }
+//         buttons {
+//           callToAction
+//         }
+//         tagline {
+//           heading
+//           text
+//         }
+//         graphics {
+//           image
+//           imageDescription
+//           heading
+//           text
+//         }
+//         section {
+//           heading
+//           text
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export default Template;
+// export default Template;
