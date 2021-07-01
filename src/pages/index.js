79757e3406ccaa03800b1f8c9af7a1ed8ec07eb5
Saiful Islam <nysaifulislam@gmail.com>
2021-07-01 10:32:48 -0400

    removed footer from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,6 @@ import i18next from "../components/i18n";
 import "@trussworks/react-uswds/lib/uswds.css";
 import "@trussworks/react-uswds/lib/index.css";
 
-/* TODO: imports for footer – remove when footer is moved to Footer.js */
-import {
-  Address,
-  Footer as FooterUSWDS,
-  FooterNav,
-  Grid,
-  Logo,
-  SocialLinks,
-} from "@trussworks/react-uswds";
-import Link from "../components/Link";
-
-import logoImg from "../../public/static/favicon.ico";
-
 function Home() {
   return (
     <Suspense fallback="loading">
@@ -34,106 +21,7 @@ function Home() {
           accuracy, relevance, timeliness or completeness of the information
           contained in non-government website links.
         </div>
-
-        {/* TODO: Footer to be added later to Footer.js */}
-        <React.Fragment>
-          <FooterUSWDS
-            size="medium"
-            // returnToTop={returnToTop}
-            primary={
-              <FooterNav
-                size="medium"
-                links={[
-                  <Link className="usa-footer__primary-link" to="#">
-                    Primary Link
-                  </Link>,
-                  <Link className="usa-footer__primary-link" to="#">
-                    Primary Link
-                  </Link>,
-                  <Link className="usa-footer__primary-link" to="#">
-                    Primary Link
-                  </Link>,
-                  <Link className="usa-footer__primary-link" to="#">
-                    Primary Link
-                  </Link>,
-                ]}
-              />
-            }
-            secondary={
-              <Grid row gap>
-                <Logo
-                  size="medium"
-                  image={
-                    <img
-                      className="usa-footer__logo-img"
-                      // alt=""
-                      src={logoImg}
-                    />
-                  }
-                  heading={
-                    <p className="usa-footer__logo-heading">Name of Agency</p>
-                  }
-                />
-                <Grid
-                  className="usa-footer__contact-links"
-                  mobileLg={{ col: 6 }}
-                >
-                  <SocialLinks
-                    links={[
-                      <Link
-                        key="facebook"
-                        className="usa-social-link--facebook"
-                        to="#"
-                      >
-                        <span>Facebook</span>
-                      </Link>,
-                      <Link
-                        key="twitter"
-                        className="usa-social-link--twitter"
-                        to="#"
-                      >
-                        <span>Twitter</span>
-                      </Link>,
-                      <Link
-                        key="youtube"
-                        className="usa-social-link--youtube"
-                        to="#"
-                      >
-                        <span>YouTube</span>
-                      </Link>,
-                      <Link
-                        key="instagram"
-                        className="usa-social-link--instagram"
-                        to="#"
-                      >
-                        <span>Instagram</span>
-                      </Link>,
-                      <Link key="rss" className="usa-social-link--rss" to="#">
-                        <span>RSS</span>
-                      </Link>,
-                    ]}
-                  />
-                  <h3 className="usa-footer__contact-heading">
-                    Agency Contact Center
-                  </h3>
-                  <Address
-                    size="medium"
-                    items={[
-                      <Link key="phone" to="tel:1-800-555-5555">
-                        (800) CALL-GOVT
-                      </Link>,
-                      <Link key="email" to="mailto:info@agency.gov">
-                        info@agency.gov
-                      </Link>,
-                    ]}
-                    // role=""
-                    // about=""
-                  />
-                </Grid>
-              </Grid>
-            }
-          ></FooterUSWDS>
-        </React.Fragment>
+        <Footer />
       </I18nextProvider>
     </Suspense>
   );
