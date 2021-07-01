5a4288f2f810741bea37e032a38e904aa3858d0e
Anya <adunaif@cto.nyc.gov>
2021-07-01 10:00:44 -0400

    add layout to landing page template

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
-export {default as Graphic} from './graphic'
-export {default as Header} from './header'
-export {default as Hero} from './hero'
-export {default as Section} from './section'
-export {default as Tagline} from './tagline'
+export { default as Graphic } from "./graphic";
+export { default as Header } from "./header";
+export { default as Hero } from "./hero";
+export { default as i18next } from "./i18n";
+export { default as Layout } from "./Layout";
+export { default as Section } from "./section";
+export { default as Tagline } from "./tagline";
