b27493dee7910bf6dae223c2671a807c9a559c1b
Anya <adunaif@cto.nyc.gov>
2021-07-01 10:57:00 -0400

    install react helmet, change title on language change

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import {
   ExtendedNav,
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Graphic } from "./graphic";
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Hero } from "./hero";
-export { default as i18next } from "./i18n";
+export { default as i18n } from "./i18n";
 export { default as LanguageSwitcher } from "./languageSwitcher";
 export { default as Layout } from "./layout";
 export { default as Link } from "./link";
