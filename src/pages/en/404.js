e4143d99eb7fade21d180f15b8c6a261d0e72026
Saiful Islam <nysaifulislam@gmail.com>
2021-07-09 12:48:42 -0400

    added layout to 404 pages

--- src/pages/en/404.js
+++ src/pages/en/404.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+// import { Layout, i18next } from "../../components";
+import Layout from "../../components/layout";
 import i18next from "../../components/i18n";
 
 function NotFound() {
   const { t } = useTranslation();
-  return <p>{t("notFound")}</p>;
+  return <Layout slug="404">{t("notFound")}</Layout>;
 }
 
 export default NotFound;
