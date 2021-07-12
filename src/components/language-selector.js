ca3f9b3f5221cd447771ccf5ca0414ccb24628d5
Anya <adunaif@cto.nyc.gov>
2021-07-12 10:28:35 -0400

    lang constants file

--- src/components/language-selector.js
+++ src/components/language-selector.js
@@ -1,53 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { navigate } from "gatsby";
 import { Grid, Link, Menu, NavDropDownButton } from "@trussworks/react-uswds";
-import globe from "../images/logos/globe.png";
+import FeatherIcon from "feather-icons-react";
+import languages from "../constants/languages";
 
 function LanguageSelector() {
   const [isOpen, setIsOpen] = useState(false);
 
-  //Hard coding here for now - will update soon
-  const testMenuItemsOne = [
-    <Link to="/en/landing" key="en">
-      English
-    </Link>,
-    <Link to="/es/landing" key="es">
-      Español
-    </Link>,
-    <Link to="/en/landing" key="en">
-      שידיי
-    </Link>,
-    <Link to="/en/landing" key="en">
-      Polski
-    </Link>,
-    <Link to="/en/landing" key="en">
-      Français
-    </Link>,
-    <Link to="/en/landing" key="en">
-      中文
-    </Link>,
-    <Link to="/en/landing" key="en">
-      繁体中文
-    </Link>,
-    <Link to="/en/landing" key="en">
-      ودرا
-    </Link>,
-    <Link to="/en/landing" key="en">
-      ةيبرعلا
-    </Link>,
-    <Link to="/en/landing" key="en">
-      Русский
-    </Link>,
-    <Link to="/en/landing" key="en">
-      বাংলা
-    </Link>,
-    <Link to="/en/landing" key="en">
-      한국어
-    </Link>,
-    <Link to="/en/landing" key="en">
-      Kreyòl ayisyen
-    </Link>,
-  ];
+  const languageMenuItems = languages.map((language) => (
+    <Link to={`/${language.langKey}`} key={language.langKey}>
+      {language.lang}
+    </Link>
+  ));
 
   return (
     <Grid>
@@ -59,14 +23,14 @@ function LanguageSelector() {
         isOpen={isOpen}
         label={
           <div className="banner__logo font-heading-xs">
-            <img src={globe} />
+            <FeatherIcon icon="globe" />
             <p>Language</p>
           </div>
         }
       />
       <Menu
         key="one"
-        items={testMenuItemsOne}
+        items={languageMenuItems}
         isOpen={isOpen}
         id="languageSelector"
       />
