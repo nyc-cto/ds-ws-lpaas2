a153df63570e53b0aeb84082f9f6e4470fec7298
Anya <adunaif@cto.nyc.gov>
2021-07-14 10:28:44 -0400

    remove language switcher in favor of language selector

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,6 @@ export { default as Header } from "./header";
 export { default as Hero } from "./hero";
 export { default as i18next } from "./i18n";
 export { default as LanguageSelector } from "./language-selector";
-export { default as LanguageSwitcher } from "./language-switcher";
 export { default as Layout } from "./layout";
 export { default as Link } from "./link";
 export { default as Section } from "./section";
