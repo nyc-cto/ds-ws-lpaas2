036ec0e515b49abc758b3ed165a6699811182cf9
Saiful Islam <nysaifulislam@gmail.com>
2021-07-16 11:19:22 -0400

    storing links in /src/constants/links.js

---
+++ src/constants/links.js
@@ -0,0 +1,11 @@
+export const header = {
+  navDropdowns: [
+    ["link-one", "link-two"],
+    ["link-three", "link-four"],
+  ],
+  parent: ["link-five"],
+};
+
+export const footer = {
+  primary: ["/link-one", "/link-two", "/link-three", "/link-four"],
+};
