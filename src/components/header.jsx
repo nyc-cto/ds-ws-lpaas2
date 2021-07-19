0d8e5ea57d79f3d0388b63561e3bb37d14332e8d
Saiful Islam <nysaifulislam@gmail.com>
2021-07-19 15:11:05 -0400

    store navigation dropdown related items entirely in src/components/nav-drop-down.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,65 +21,6 @@ function Header({ slug }) {
   const [expanded, setExpanded] = useState(false);
   const onClick = () => setExpanded((prevExpanded) => !prevExpanded);
 
-  /* dynamically store dropdowns in navigation bar */
-  const dropdowns = [];
-  const dropdownLinks = links.navDropdowns;
-  const constFileLength = dropdownLinks.length;
-  const dropdownLabels = t('header.nav.dropdowns');
-  const translationFileLength = dropdownLabels.length;
-  const length = translationFileLength > constFileLength
-    ? constFileLength
-    : translationFileLength;
-  /* take shorter length in case there is missing dropdowns in
-      `../constants/links.js` (constants file) or `../locales/` (translation files) */
-  if (translationFileLength !== constFileLength) {
-    console.error(
-      'Different number of dropdowns in /src/constants/link.js and dropdown labels in /src/locales\n',
-      `${constFileLength} dropdown${
-        constFileLength > 1 ? 's' : ''
-      } in /src/constants/link.js\n`,
-      `${translationFileLength} dropdown${
-        translationFileLength > 1 ? 's' : ''
-      } in /src/locales`,
-    );
-  }
-  // eslint-disable-next-line array-callback-return
-  dropdownLinks.map((_, i) => {
-    if (i < length) {
-      const navDropdownLinks = dropdownLinks[i];
-      const navDropdownLinksLength = navDropdownLinks.length;
-      const navDropdownLinkLabels = t('header.nav.dropdowns')[i].simpleLinks;
-      const navDropdownLinkLabelsLength = navDropdownLinkLabels.length;
-      const navDropdownLength = navDropdownLinksLength > navDropdownLinkLabelsLength
-        ? navDropdownLinkLabelsLength
-        : navDropdownLinksLength;
-      /* take shorter length in case there is a missing link
-        in `../constants/links.js` (constants file) or label in `../locales/` (translation files) */
-      if (navDropdownLinksLength !== navDropdownLinkLabelsLength) {
-        console.error(
-          `Different number of links in /src/constants/link.js and link labels in /src/locales for dropdown ${
-            i + 1
-          }\n`,
-          'Links: ',
-          navDropdownLinks,
-          '\n',
-          'Link labels: ',
-          navDropdownLinkLabels,
-          '\n',
-        );
-      }
-      dropdowns.push(
-        navDropdownLinks.map(
-          (element, j) => j < navDropdownLength && (
-          <Link to={element} key={element}>
-            {navDropdownLinkLabels[j]}
-          </Link>
-          ),
-        ),
-      );
-    } else dropdowns.push([]);
-  });
-
   /* dynamically store parent links */
   const parentLinks = links.parent;
   const parentLinksLength = parentLinks.length;
@@ -121,7 +62,7 @@ function Header({ slug }) {
         <NavMenuButton onClick={onClick} label="Menu" />
       </div>
       <ExtendedNav
-        primaryItems={NavDropDown(dropdowns).concat(parentLinkItems)}
+        primaryItems={NavDropDown().concat(parentLinkItems)}
         secondaryItems={[]}
         mobileExpanded={expanded}
         onToggleMobileNav={onClick}
