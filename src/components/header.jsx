9121a56b12b92f5f857a1152817f80e9811a0704
Anya <adunaif@cto.nyc.gov>
2021-07-21 14:46:19 -0400

    refactor language nav items

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -60,40 +60,40 @@ function Header({ slug }) {
   const languageNavItems = (
     <div className="header__language-nav-container">
       <div className="header__language-nav-RtoL">
-        {languages
-          .filter((language) => language.isRtoL)
-          .map((language) => (
-            <div className="header__language-nav-items">
-              <Button
-                onClick={() => {
-                  handleClick(language.langKey);
-                }}
-                type="button"
-                unstyled
-                key={language.langKey}
-              >
-                {language.lang}
-              </Button>
-            </div>
-          ))}
+        {languages.map(
+          (language) => language.isRtoL && (
+          <div className="header__language-nav-items">
+            <Button
+              onClick={() => {
+                handleClick(language.langKey);
+              }}
+              type="button"
+              unstyled
+              key={language.langKey}
+            >
+              {language.lang}
+            </Button>
+          </div>
+          ),
+        )}
       </div>
       <div className="header__language-nav-LtoR">
-        {languages
-          .filter((language) => !language.isRtoL)
-          .map((language) => (
-            <div className="header__language-nav-items">
-              <Button
-                onClick={() => {
-                  handleClick(language.langKey);
-                }}
-                type="button"
-                unstyled
-                key={language.langKey}
-              >
-                {language.lang}
-              </Button>
-            </div>
-          ))}
+        {languages.map(
+          (language) => !language.isRtoL && (
+          <div className="header__language-nav-items">
+            <Button
+              onClick={() => {
+                handleClick(language.langKey);
+              }}
+              type="button"
+              unstyled
+              key={language.langKey}
+            >
+              {language.lang}
+            </Button>
+          </div>
+          ),
+        )}
       </div>
     </div>
   );
