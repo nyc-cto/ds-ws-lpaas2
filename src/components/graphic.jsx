e6ac718e5604670c1924e2876bf822b19f9d65e1
Anya <adunaif@cto.nyc.gov>
2021-07-29 15:17:45 -0400

    language selector styling

--- src/components/graphic.jsx
+++ src/components/graphic.jsx
@@ -9,36 +9,36 @@ function Graphic({ graphics }) {
       <GridContainer>
         <Grid>
           <Grid row gap className="usa-graphic-list__row">
-            {graphics &&
-              graphics.map((graphic) => (
-                <Grid
-                  className="usa-media-block"
-                  row
-                  gap
-                  desktop={{ col: 6 }}
-                  key={graphic.heading}
-                >
-                  <Grid className="graphic__image-container" tablet={{ col: 3 }} desktop={{ col: 5 }}>
-                    {graphic.image && (
-                      <img
-                        className="graphic__image--circle usa-media-block__img"
-                        src={graphic.image}
-                        alt={graphic.imageDescription}
-                      />
-                    )}
-                  </Grid>
-                  <Grid className="usa-media-block__body" tablet={{ col: 9 }} desktop={{ col: 7 }}>
-                    {graphic.heading && (
-                      <h2 className="usa-graphic-list__heading">
-                        {graphic.heading}
-                      </h2>
-                    )}
-                    {graphic.text && (
-                      <p className="usa-prose">{graphic.text}</p>
-                    )}
-                  </Grid>
+            {graphics
+            && graphics.map((graphic) => (
+              <Grid
+                className="usa-media-block"
+                row
+                gap
+                desktop={{ col: 6 }}
+                key={graphic.heading}
+              >
+                <Grid className="graphic__image-container" tablet={{ col: 3 }} desktop={{ col: 5 }}>
+                  {graphic.image && (
+                  <img
+                    className="graphic__image--circle usa-media-block__img"
+                    src={graphic.image}
+                    alt={graphic.imageDescription}
+                  />
+                  )}
                 </Grid>
-              ))}
+                <Grid className="usa-media-block__body" tablet={{ col: 9 }} desktop={{ col: 7 }}>
+                  {graphic.heading && (
+                  <h2 className="usa-graphic-list__heading">
+                    {graphic.heading}
+                  </h2>
+                  )}
+                  {graphic.text && (
+                  <p className="usa-prose">{graphic.text}</p>
+                  )}
+                </Grid>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
       </GridContainer>
