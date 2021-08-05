5fb794662366d6ce560db348866a0eaa5e09a8d9
Saiful Islam <nysaifulislam@gmail.com>
2021-08-05 17:48:24 -0400

    inline-documentation for src/constants/links.js

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,13 +1,13 @@
 export const header = {
-  navDropDownLinks: [
-    ['link-one', 'link-two'],
-    ['link-three', 'link-four'],
+  navDropDownLinks: [ // dropdowns
+    ['link-one', 'link-two'], // first dropdown
+    ['link-three', 'link-four'], // second dropdown
   ],
-  parentLinks: ['link-five'],
+  parentLinks: ['link-five'], // parent (standalone) links
 };
 
 export const footer = {
-  primary: header.navDropDownLinks.flat().concat(header.parentLinks),
+  primary: header.navDropDownLinks.flat().concat(header.parentLinks), // taken directly from header (don't make change here)
   secondary: {
     terms: 'terms',
     privacy: 'privacy',
