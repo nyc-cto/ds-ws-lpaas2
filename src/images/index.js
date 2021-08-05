39abeabb8916020ae1e1e4cd6dddca3c76e344f2
Anya <adunaif@cto.nyc.gov>
2021-08-05 13:48:37 -0400

    change logo-banner from png to svg

--- src/images/index.js
+++ src/images/index.js
@@ -3,7 +3,7 @@ import graphicsOne from './graphics/graphics-one.png';
 import graphicsThree from './graphics/graphics-three.png';
 import graphicsTwo from './graphics/graphics-two.png';
 
-export { default as logoBanner } from './logos/logo-banner.png';
+export { default as logoBanner } from './logos/logo-banner.svg';
 export { default as logoHeader } from './logos/logo-header.png';
 export { default as logoFooter } from './logos/logo-footer.png';
 
