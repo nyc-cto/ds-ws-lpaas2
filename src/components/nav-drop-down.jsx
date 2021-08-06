a826a7db84d38c13b6147a44a8cadd0387624219
Saiful Islam <nysaifulislam@gmail.com>
2021-08-06 15:26:26 -0400

    updating src/components/nav-drop-down.jsx with object changes in src/constants/links.js

--- src/components/nav-drop-down.jsx
+++ src/components/nav-drop-down.jsx
@@ -12,18 +12,14 @@ function NavDropDown() {
 
   /* dynamically store dropdowns */
   const dropdowns = [];
-  const dropDownLinks = links.navDropDownLinks;
-  const constFileLength = dropDownLinks.length;
-  const dropDownLabels = t('navigation.dropdowns');
-  const translationFileLength = dropDownLabels.length;
-  const length = translationFileLength > constFileLength
-    ? constFileLength
-    : translationFileLength;
-  /* take shorter length in case there is missing dropdowns in
-      `../constants/links.js` (constants file) or `../locales/` (translation files) */
+  const { navDropDowns } = links; // links + labels
+  const constFileLength = navDropDowns.length;
+  const navDropDownLabels = t('navigation.dropdowns'); // labels
+  const translationFileLength = navDropDownLabels.length;
+  // error message if there is missing dropdowns or dropdown labels
   if (translationFileLength !== constFileLength) {
     console.error(
-      'Different number of dropdowns in /src/constants/link.js (under header.navDropDownLinks) and labels in /src/locales (under navigation.dropdowns)\n',
+      'Different number of dropdowns in /src/constants/link.js (under header.navDropDowns) and labels in /src/locales (under navigation.dropdowns)\n',
       `${constFileLength} dropdown${
         constFileLength > 1 ? 's' : ''
       } in /src/constants/link.js\n`,
@@ -32,75 +28,68 @@ function NavDropDown() {
       } in /src/locales`,
     );
   }
-  dropDownLinks.forEach((_, i) => {
-    if (i < length) {
-      const navDropDownLinks = dropDownLinks[i];
-      const navDropDownLinksLength = navDropDownLinks.length;
-      const navDropDownLinkLabels = dropDownLabels[i].linkLabels;
-      const navDropDownLinkLabelsLength = navDropDownLinkLabels.length;
-      const navDropDownLength = navDropDownLinksLength > navDropDownLinkLabelsLength
-        ? navDropDownLinkLabelsLength
-        : navDropDownLinksLength;
-      /* take shorter length in case there is a missing link
-        in `../constants/links.js` (constants file) or label in `../locales/` (translation files) */
-      if (navDropDownLinksLength !== navDropDownLinkLabelsLength) {
-        console.error(
-          `Different number of links in /src/constants/link.js (under header.navDropDownLinks) and labels in /src/locales (under navigation.dropdowns) for dropdown ${
-            i + 1
-          }\n`,
-          'Links: ',
-          navDropDownLinks,
-          '\n',
-          'Link labels: ',
-          navDropDownLinkLabels,
-          '\n',
-        );
-      }
-      dropdowns.push(
-        navDropDownLinks.map(
-          (element, j) => j < navDropDownLength && (
-          <Link to={element} key={element}>
-            {navDropDownLinkLabels[j]}
-          </Link>
-          ),
-        ),
+  navDropDowns.forEach((navDropDown, i) => {
+    const navDropDownLinksLength = navDropDown.length;
+    const navDropDownLinkLabels = navDropDownLabels[i].linkLabels;
+    const navDropDownLinkLabelsLength = navDropDownLinkLabels.length;
+    console.log(links.navDropDowns[0]);
+    if (navDropDownLinksLength !== navDropDownLinkLabelsLength) {
+      console.error(
+        `Different number of links in /src/constants/link.js (under header["navDropDowns"][${i}]) and labels in /src/locales (under navigation["dropdowns"][${i}]["linkLabels"]) for dropdown ${
+          i + 1
+        }\n`,
+        'Links: ',
+        navDropDown,
+        '\n',
+        'Link labels: ',
+        navDropDownLinkLabels,
+        '\n',
       );
-    } else dropdowns.push([]);
+    }
+    dropdowns.push(
+      navDropDown.map((linkAndLabel, _) => (
+        <Link to={linkAndLabel.link} key={_}>
+          {t(linkAndLabel.label)}
+        </Link>
+      )),
+    );
   });
 
   /* dynamically create dropdowns */
   const [isOpen, setIsOpen] = useState(undefined);
-  const navBarItems = dropdowns
-    .map((_, i) => {
-      const id = `navDropDown${i}`;
-      const iStr = i.toString();
-      /* dynamically create dropdowns in navigation bar */
-      return (
-        <>
-          <NavDropDownButton
-            onToggle={() => {
-              if (isOpen === id) setIsOpen(undefined);
-              else setIsOpen(id);
-            }}
-            menuId={id}
-            isOpen={isOpen === id}
-            label={(
-              <div className="nav-dropdown__label">
-                <p>{dropDownLabels[i].buttonLabel}</p>
-                <FeatherIcon className="nav-dropdown__label-icon" icon="chevron-down" size={16} />
-                <FeatherIcon className="nav-dropdown__label-icon--expanded" icon="chevron-up" color="white" size={16} />
-              </div>
-            )}
-          />
-          <Menu
-            id={id}
-            isOpen={isOpen === id}
-            items={dropdowns[i]}
-            key={iStr}
-          />
-        </>
-      );
-    });
+  const navBarItems = dropdowns.map((_, i) => {
+    const id = `navDropDown${i}`;
+    /* dynamically create dropdowns in navigation bar */
+    return (
+      <>
+        <NavDropDownButton
+          onToggle={() => {
+            if (isOpen === id) setIsOpen(undefined);
+            else setIsOpen(id);
+          }}
+          menuId={id}
+          isOpen={isOpen === id}
+          label={(
+            <div className="nav-dropdown__label">
+              <p>{navDropDownLabels[i].buttonLabel}</p>
+              <FeatherIcon
+                className="nav-dropdown__label-icon"
+                icon="chevron-down"
+                size={16}
+              />
+              <FeatherIcon
+                className="nav-dropdown__label-icon--expanded"
+                icon="chevron-up"
+                color="white"
+                size={16}
+              />
+            </div>
+          )}
+        />
+        <Menu id={id} isOpen={isOpen === id} items={dropdowns[i]} key={i} />
+      </>
+    );
+  });
   return navBarItems;
 }
 
