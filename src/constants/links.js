21577108faf4fb40287812e59e0ff02536534967
Saiful Islam <nysaifulislam@gmail.com>
2021-08-06 15:25:35 -0400

    using objects in src/constants/links.js for seeing relation between label in translation file and link in constant file

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,13 +1,33 @@
 export const header = {
-  navDropDownLinks: [
-    ['link-one', 'link-two'],
-    ['link-three', 'link-four'],
+  navDropDowns: [
+    [
+      {
+        label: 'navigation.dropdowns.0.linkLabels.0',
+        link: 'link-one',
+      },
+      {
+        label: 'navigation.dropdowns.0.linkLabels.1',
+        link: 'link-two',
+      },
+    ],
+    [
+      {
+        label: 'navigation.dropdowns.1.linkLabels.0',
+        link: 'link-three',
+      },
+      {
+        label: 'navigation.dropdowns.1.linkLabels.1',
+        link: 'link-four',
+      },
+    ],
   ],
-  parentLinks: ['link-five'],
+  parentLinks: [{ label: 'navigation.parentLinkLabels.0', link: 'link-five' }],
 };
 
 export const footer = {
-  primary: header.navDropDownLinks.flat().concat(header.parentLinks),
+  primary: header.navDropDowns
+    .flat() // turn nested array into flat/unnested array
+    .concat(header.parentLinks), // add parent links
   secondary: {
     terms: 'terms',
     privacy: 'privacy',
