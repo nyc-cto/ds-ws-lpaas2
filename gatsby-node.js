f2c9d3fe176b2df3668c8000c7ec5e95b5359b5b
Saiful Islam <nysaifulislam@gmail.com>
2021-08-10 17:39:01 -0400

    404 routing in gatsby-node - not working as of yet

--- gatsby-node.js
+++ gatsby-node.js
@@ -90,6 +90,24 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 };
 
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+
+  // Check if the page is a localized 404 (i.e. /en/404/)
+  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
+    const oldPage = { ...page };
+
+    // Get the language code from the path, and match all paths
+    // starting with this code (apart from other valid paths)
+    const langCode = page.path.split('/')[1];
+    page.matchPath = `/${langCode}/*`;
+
+    // Recreate the modified page
+    deletePage(oldPage);
+    createPage(page);
+  }
+};
+
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     node: {
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,6 +4,7 @@ import { useLocation } from '@reach/router';
 import { navigate } from 'gatsby';
 import { useTranslation } from 'react-i18next';
 
+import { languages } from '../constants/languages';
 import Footer from './footer';
 import Header from './header';
 
@@ -14,14 +15,14 @@ function Layout({ children, slug }) {
   useEffect(() => {
     const path = location.pathname;
     const lang = path.split('/')[1]; // language
-    const route = path
-      .split('/')
-      .slice(2)
-      .filter((v) => v !== '')
-      .join('/'); // rest of the path excluding the language
-    if (path === '/') navigate(`/${i18n.language}/`); // '/' redirect
-    else if (lang === '404') navigate(`/${i18n.language}/404`); // '/404' redirect
-    else if (lang !== i18n.language) i18n.changeLanguage(lang); // change current language if there's a different language entered in path
+    if (path === '/') navigate(`/${i18n.language}/`);
+    // '/' redirect
+    else if (path === '/404/') navigate(`/${i18n.language}/404`);
+    // '/404' redirect
+    else if (
+      lang !== i18n.language // if there's a different language entered in path
+      && languages.map((language) => language.langKey).includes(lang) // and the language exists in this app
+    ) i18n.changeLanguage(lang); // then change the current language to that language
   }, [location.pathname]);
 
   return (
