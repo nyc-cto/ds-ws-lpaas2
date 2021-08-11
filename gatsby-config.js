ced978d032f3891c4c4066cccdeb00440ab1563a
Anya <adunaif@cto.nyc.gov>
2021-08-11 15:31:20 -0400

    fix linting error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
-const activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   // import .env.development or .env.production based on environment
